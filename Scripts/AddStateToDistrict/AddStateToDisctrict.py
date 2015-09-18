250490431aaeefa3e94fc1c99480bc54c3a2116c
kovacicek <kovacicek@hotmail.com>
2015-09-18 10:09:01 +0200

    cleanup

--- Scripts/AddStateToDistrict/AddStateToDisctrict.py
+++ Scripts/AddStateToDistrict/AddStateToDisctrict.py
@@ -16,14 +16,16 @@ http://pandas.pydata.org/pandas-docs/dev/generated/pandas.DataFrame.to_excel.htm
 http://pandas.pydata.org/pandas-docs/dev/generated/pandas.DataFrame.sort.html
 """
 
-from os import path, listdir
+from os import listdir
+from os.path import join, splitext
 from pandas import ExcelWriter, read_csv, concat
 
-#Directory containing .csv files that will be concatenated
-DataDirState = "InputFiles/_AEIS_State"
-DataDirDistrict = "InputFiles/AEIS_District"
 
 class AddStateRow:
+    # Directory containing .csv files that will be concatenated
+    data_dir_state = "InputFiles/_AEIS_State"
+    data_dir_district = "InputFiles/AEIS_District"
+
     def __init__(self):
         self.ReadData()
         #self.SortData()
@@ -50,30 +52,40 @@ class AddStateRow:
 
             data_frames = list()
             # List directory containing the .csv files
-            for district_item in listdir(DataDirDistrict):
-                #Check the extension of the files, so only .csv files will be considered
-                if path.splitext(district_item)[1] == ".csv":
-                    f = path.join(DataDirDistrict,district_item)
-                    #ovde bih trebao provjeriti fajlove cija imena se poklapaju
-                    full_file_name_district = path.splitext(district_item)[0]
-                    
-                    for state_item in listdir(DataDirState):
-                        #Check the extension of the files, so only .csv files will be considered
-                        if path.splitext(state_item)[1] == ".csv":
-                            f = path.join(DataDirState,state_item)
-                            #get full name of the state file
-                            full_file_name_state = path.splitext(state_item)[0]
-                            
-                            if full_file_name_district[:5] == full_file_name_state[:5] :
-                                if full_file_name_district[14:] == full_file_name_state[11:] :
-                                    print(full_file_name_district)
-                                    print(full_file_name_state)
+            for district_item in listdir(self.data_dir_district):
+                # Check only .csv files
+                if splitext(district_item)[1] == ".csv":
+                    district_file_path = join(self.data_dir_district,
+                                              district_item)
+                    # ovde bih trebao provjeriti fajlove cija imena se poklapaju
+                    full_file_name_district = splitext(district_item)[0]
+
+                    # Find proper state file
+                    self.FindProperStateFile(full_file_name_district)
                     
                     #Pandas.read_csv method returns DataFrame object, so we append that object to the data_frames list
                     #data_frames.append(read_csv(f, usecols=Columns, delimiter=",", header=0))
             #self.data = concat(data_frames)  
     #end ReadData
-
+    def FindProperStateFile(self, full_file_name_district):
+        """
+        
+        """
+        for state_item in listdir(self.data_dir_state):
+            # Check only .csv files
+            if path.splitext(state_item)[1] == ".csv":
+                state_file_path = path.join(self.data_dir_state, state_item)
+                #get full name of the state file
+                full_file_name_state = path.splitext(state_item)[0]
+                tmp = full_file_name_state.replace("state", "district")
+                print(tmp)
+                if full_file_name_district == tmp:
+                    print(full_file_name_district, full_file_name_state)
+#                         print(full_file_name_state)
+#                 if full_file_name_district[:5] == full_file_name_state[:5]:
+#                     if full_file_name_district[14:] == full_file_name_state[11:]:
+#                         print(full_file_name_district)
+#                         print(full_file_name_state)
 def main():
     AddStateRow()
     print("Finished")
