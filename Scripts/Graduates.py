c9b45da4370b1ac41b3a204c7937f85eaa9f2fb8
Drazen <drazenpezer@gmail.com>
2015-09-30 00:00:28 +0200

    Updated SAT_ACT.py and Graduates.py scripts

--- Scripts/Graduates.py
+++ Scripts/Graduates.py
@@ -9,28 +9,30 @@ Created on 24.09.2015.
 
 from os import path, listdir, mkdir, remove
 from os.path import join, splitext, exists
-from pandas import ExcelWriter, read_csv, concat
+from pandas import ExcelWriter, read_csv, concat, merge
+from pandas.core.frame import DataFrame
 
 # Columns that will be extracted from the files
-Columns = ["AGC4*YY*R",
-           "3GC4*YY*R",
-           "RGC4*YY*R",
-           "EGC4*YY*R",
-           "FGC4*YY*R",
-           "HGC4*YY*R",
-           "LGC4*YY*R",
-           "MGC4*YY*R",
-           "IGC4*YY*R",
-           "4GC4*YY*R",
-           "SGC4*YY*R",
-           "2GC4*YY*R",
-           "WGC4*YY*R"
+Columns = ["AGC4*R",
+           "3GC4*R",
+           "RGC4*R",
+           "EGC4*R",
+           "FGC4*R",
+           "HGC4*R",
+           "LGC4*R",
+           "MGC4*R",
+           "IGC4*R",
+           "4GC4*R",
+           "SGC4*R",
+           "2GC4*R",
+           "WGC4*R"
            ]
 
 DS = {'district': 'D',
       'campus': 'C'}
 
-
+# (name_parts[2] == "completion" 
+#                            or name_parts[2] == "completioner"))
 class Graduates:
     script_name = "Graduates"
     # inputs = {ds : (input_dir, output_dir)}
@@ -39,6 +41,7 @@ class Graduates:
               "district": (join("..", "Inputs", "DistrictState"),
                            "%sDistrict" % script_name)
              }
+    columns_dict = dict()
 
     def __init__(self):
         self.CleanOutput()
@@ -52,7 +55,8 @@ class Graduates:
         # Read Inputs
         for key, value in self.inputs.items():
             data_frames = list()
-            
+
+            # read generated files and append those data frames to the list
             for item in listdir(value[1]):
                 if path.splitext(item)[1] == ".csv":
                     f = path.join(value[1], item)
@@ -60,36 +64,64 @@ class Graduates:
                                                 delimiter=",",
                                                 header=0,
                                                 low_memory=False))
-            # Merge data
+
+            # Merge data frames into one data frame
             data = data_frames[0]
             for item in data_frames[1:]:
                 data = data.append(item)
-    
+
             # Write to output
             merged_file = join("..",
                                "MergedFiles",
                                "%s_%s.csv" % (self.script_name, key))
-            data.to_csv(merged_file, sep=",", index = False)
+            print("Merged file: " + merged_file)
+
+            # get output columns from self.columns_dict dictionary
+            cols = self.GetOutputCols(ds=key) 
+            
+            # write merged data frame to csv file
+            data.to_csv(merged_file, sep=",", index = False, columns=cols)
             print("\t %s files merged into: %s" % (key, merged_file))
     # end Merge
+
+    def GetOutputCols(self, ds):
+        """
+        From self.columns_dict dictionary extract columns that will be written
+        to the output file. This is being done because of writing order
+        """
+        a = list()
+        a.append("DISTRICT" if ds == 'district' else "CAMPUS")
+        a.append("YEAR")
+        b= [x for x in set(self.columns_dict.values()) if
+                (x[0].lower() == ds[0].lower())]
+        return a + b
+    # end GetOutputCols
 
     def AdjustColumn(self, ds, year=None):
         adjusted_columns = list()
         # add district/campus and year to columns
         if ds == 'district':
             adjusted_columns.append('DISTRICT')
+            # self.columns_dict['DISTRICT'] = 'DISTRICT'
         elif ds == 'campus':
             adjusted_columns.append('CAMPUS')
+            # self.columns_dict['CAMPUS'] = 'CAMPUS'
         else:
             print("Can't determine district or campus: ds = %" % ds)
         adjusted_columns.append('YEAR')
+        self.columns_dict['YEAR'] = 'YEAR'
+        # sometimes in files stands year
+        adjusted_columns.append('year')
+        self.columns_dict['year'] = 'YEAR'
 
         for column in Columns:
             if ds is not None:
                 column = DS[ds] + column
+                key = column
             if year is not None:
-                column = column.replace('*YY*', year)
+                column = column.replace('*', year)
             adjusted_columns.append(column)
+            self.columns_dict[column] = key
         return adjusted_columns
     # end AdjustColumns
 
@@ -104,7 +136,6 @@ class Graduates:
 
     def ReadData(self):
         print("\nRead Data")
-        
         for ds, value in self.inputs.items():
             if not path.exists(value[0]):
                 print("\t Data Directory Does Not Exist %s" % value[0])
@@ -116,26 +147,39 @@ class Graduates:
                     # so only .csv files will be considered
                     name_of_file = path.splitext(item)[0]
                     name_parts = name_of_file.split("_")
-                    name_year = str(int(name_parts[0]) - 1)
-    
+                    name_year = str(int(name_parts[0]) - 1)[2:4]
+
                     if(path.splitext(item)[1] == ".csv" 
                        and (name_parts[2] == "completion" 
                             or name_parts[2] == "completioner")):
                         file_path = path.join(value[0], item)
-                        adjusted_columns = self.AdjustColumn(ds=ds, year=name_year[2:4])
+                        adjusted_columns = self.AdjustColumn(ds=ds,
+                                                             year=name_year)
                         self.adjusted = adjusted_columns
                         
-                        # Pandas.read_csv method returns DataFrame object
-                        try:
-                            data_frame = read_csv(file_path,
-                                              usecols=adjusted_columns,
-                                              delimiter=",",
-                                              header=0,
-                                              low_memory=False)
-                            self.WriteData(data_frame, item, value[1])
-                        except:
-                            print("Error while reading %s" % item)
-                            print("Columns: %s\n" % adjusted_columns)
+                        data_frames = list()
+                        for col in adjusted_columns:
+                            try:
+                                data_f = read_csv(file_path,
+                                                  usecols=[col],
+                                                  delimiter=",",
+                                                  header=0,
+                                                  low_memory=False)
+                                data_frames.append(data_f)
+                            except:
+                                # year column in files can be YEAR or year
+                                if col.lower() != 'year':
+                                    print("No such column %s" % col)
+                        data_frame = data_frames[0]
+                        for right_frame in data_frames[1:]:
+                            data_frame = merge(data_frame,
+                                               right_frame,
+                                               left_index=True,
+                                               right_index=True,
+                                               sort=False)
+                        data_frame.rename(columns=self.columns_dict,
+                                          inplace=True)
+                        self.WriteData(data_frame, item, value[1])
     # end ReadData
 
     def WriteData(self,
