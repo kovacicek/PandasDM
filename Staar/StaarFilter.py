abce8a4165b8d406f6f2787536203c15eba84b1a
kovacicek <kovacicek@hotmail.com>
2015-10-26 23:06:43 +0100

    scripts

--- Staar/StaarFilter.py
+++ Staar/StaarFilter.py
@@ -1,5 +1,5 @@
 '''
-Created on 19.10.2015.
+Created on 22.10.2015.
 
 @author: Milan Kovacic
 @e-mail: kovacicek@hotmail.com
@@ -8,12 +8,18 @@ Created on 19.10.2015.
 '''
 
 from os import path, listdir, mkdir, remove
-from os.path import join, splitext, exists, basename
-from pandas import ExcelWriter, read_csv, concat
+from os.path import join, splitext, exists
+from pandas import ExcelWriter, read_csv, concat, merge
+from pandas.core.frame import DataFrame
+#from bokeh.sampledata.stocks import filename
 
+values = ["d",
+          "rs",
+          "satis_ph1_nm",
+          "satis_rec_nm"]
 
-class StaarMerge:
-    script_name = "StaarMerge"
+class StaarFilter:
+    script_name = "StaarFilter"
     data_frames = list()
 
     def __init__(self, input_dir, output_dir):
@@ -22,7 +28,6 @@ class StaarMerge:
 
         self.CleanOutput()
         self.ReadData()
-        self.Merge()
     # end __init__
 
     def CleanOutput(self):
@@ -40,49 +45,49 @@ class StaarMerge:
         print("\nRead Data")
         # List directory containing the .csv files
         for filename in listdir(self.input_dir):
-            # Check the extension of the files,
-            # so only .csv files will be considered
+
             name_of_file = path.splitext(filename)[0]
             if(path.splitext(filename)[1] == ".csv"):
                 file_path = path.join(self.input_dir, filename)
-
+                print("File path: " + file_path)
                 # Pandas.read_csv method returns DataFrame object
                 try:
-                    data_frame = read_csv(file_path,
-                                      delimiter=",",
-                                      header=0,
-                                      low_memory=False)
-                    if "district-state" in filename:
-                        data_frame.insert(0, 'DISTRICT', 1)
-                    self.data_frames.append(data_frame)
-                    print("File with name %s appended" % filename)
-                    # self.WriteData(data_frame, filename)
+                    df = read_csv(file_path,
+                                  delimiter=",",
+                                  header=0,
+                                  low_memory=False)
+                    df = df[df['Category'].isin(values)]
+                    self.WriteData(df, filename)
                 except:
                     print("Error while reading %s" % filename)
     # end ReadData
 
-    def Merge(self):
-        print("Start merging")
+    def WriteData(self,
+                  data_frame,
+                  output_name):
+        """
+        Demonstrated how to write files in .csv and .xlsx format
+        DataFrame object has methods to_csv and to_excel
+        """
         if not exists(self.output_dir):
             mkdir(self.output_dir)
-        data =  concat(self.data_frames)
+        print("\t Writing %s" % output_name)
+        data_frame.to_csv(join(self.output_dir, output_name),
+                          sep=",",
+                          index=False)
+    # end WriteData
 
-        # Write to output
-        merged_file = join(self.output_dir,
-                           "%s_merged.csv" % (basename(self.input_dir)))
-        data.to_csv(merged_file, sep=",", index = False)
-        print("\t %s files merged into: %s" % (self.input_dir, merged_file))
-    # end Merge
 
 def main():
+    staar_wide = "2_staar_wide"
     staar_filtered = "3_staar_filtered"
-    staar_merged = "4_staar_merged"
 
-    for item_dir in listdir(staar_filtered):
-        input_dir = join(staar_filtered, item_dir)
-        output_dir = join(staar_merged, item_dir)   
-        StaarMerge(input_dir, output_dir)
+    for item_dir in listdir(staar_wide):
+        input_dir = join(staar_wide, item_dir)
+        output_dir = join(staar_filtered, item_dir)   
+        StaarFilter(input_dir, output_dir)
     print("Finished")
 
 if __name__ == "__main__":
     main()
+    
